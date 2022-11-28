0fe6eb346a2d8aa0544d8638c08d793760c8bffc
pradeepcoebmotihari <54035127+pradeepcoebmotihari@users.noreply.github.com>
2022-11-28 20:39:54 +0530

    Add files via upload

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export AWS_REGION=ap-south-1
-echo "[server]" > hosts
+echo "[server]" > /etc/ansible/hosts
 for i in `aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names jenkinsasg --query 'AutoScalingGroups[*].Instances[*].InstanceId' --output text`
 do
 aws ec2 describe-instances --instance-ids $i |jq -r '.Reservations[].Instances[].PrivateIpAddress' >> /etc/ansible/hosts
