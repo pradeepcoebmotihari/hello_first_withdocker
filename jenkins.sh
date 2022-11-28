e612d38f8dcfd7ac4602fb703b100fa621da307f
pradeepcoebmotihari <54035127+pradeepcoebmotihari@users.noreply.github.com>
2022-11-28 18:56:22 +0530

    Add files via upload

---
+++ jenkins.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "[server]" > hosts
+for i in `aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names jenkinsasg --query 'AutoScalingGroups[*].Instances[*].InstanceId' --output text`
+do
+aws ec2 describe-instances --instance-ids $i |jq -r '.Reservations[].Instances[].PrivateIpAddress' >> hosts
+done
