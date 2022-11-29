adca7f55670ed315ff5095949cae05488a43e875
pradeepcoebmotihari <54035127+pradeepcoebmotihari@users.noreply.github.com>
2022-11-29 21:53:16 +0530

    Add files via upload

--- jenkins.sh
+++ jenkins.sh
@@ -5,7 +5,7 @@ for i in `aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name
 do 
 #aws autoscaling detach-instances --instance-ids $i --auto-scaling-group-name ansibleasg --should-decrement-desired-capacity
 aws ec2 describe-instances --instance-ids $i |jq -r '.Reservations[].Instances[].PrivateIpAddress' >> /etc/ansible/hosts
-ansiblePlaybook ansible.yml -u ansible --private-key=/home/id_rsa
+ansible-playbook ansible.yml -u ansible --private-key=/home/id_rsa
 sleep 330
 #aws autoscaling attach-instances --instance-ids $i --auto-scaling-group-name ansibleasg
 done
