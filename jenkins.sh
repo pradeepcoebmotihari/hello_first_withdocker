3b57ec75c88e3542e6c33570664ff46cf0daf376
pradeepcoebmotihari <54035127+pradeepcoebmotihari@users.noreply.github.com>
2022-11-30 16:26:20 +0530

    Add files via upload

--- jenkins.sh
+++ jenkins.sh
@@ -7,7 +7,7 @@ aws elbv2 deregister-targets --target-group-arn arn:aws:elasticloadbalancing:ap-
 aws ec2 describe-instances --instance-ids $i |jq -r '.Reservations[].Instances[].PrivateIpAddress' >> /etc/ansible/hosts
 ansible-playbook -e 'host_key_chaking=False' ansible.yml --private-key=/home/id_rsa -u ansible --ssh-common-args='-o StrictHostKeyChecking=no'
 sleep 330
-aws elbv2 deregister-targets --target-group-arn arn:aws:elasticloadbalancing:ap-south-1:623240248731:targetgroup/jenkinstg/9adf4a6004cdb56e --targets Id=$i
+aws elbv2 register-targets --target-group-arn arn:aws:elasticloadbalancing:ap-south-1:623240248731:targetgroup/jenkinstg/9adf4a6004cdb56e --targets Id=$i
 done
 
 
