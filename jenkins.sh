50e297d23263813679aef2d3b2680de405d0be4d
pradeepcoebmotihari <54035127+pradeepcoebmotihari@users.noreply.github.com>
2022-11-30 16:05:47 +0530

    Add files via upload

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 echo "[server]" > /etc/ansible/hosts
-for i in `aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names jenkinsasg --query 'AutoScalingGroups[*].Instances[*].InstanceId' --output text` 
-do 
-aws autoscaling detach-instances --instance-ids $i --auto-scaling-group-name jenkinsasg --should-decrement-desired-capacity
+for i in `aws elbv2 describe-target-health --target-group-arn arn:aws:elasticloadbalancing:ap-south-1:623240248731:targetgroup/jenkinstg/9adf4a6004cdb56e --query 'TargetHealthDescriptions[*].Target.Id' --output text`
+do
+aws elbv2 deregister-targets --target-group-arn arn:aws:elasticloadbalancing:ap-south-1:623240248731:targetgroup/jenkinstg/9adf4a6004cdb56e --targets Id=$i 
 aws ec2 describe-instances --instance-ids $i |jq -r '.Reservations[].Instances[].PrivateIpAddress' >> /etc/ansible/hosts
-ansible-playbook -e 'host_key_chaking=False' ansible.yml --private-key=/home/id_rsa -u ansible --ssh-common-args='-o StrictHostKeyChecking=no' 
+ansible-playbook -e 'host_key_chaking=False' ansible.yml --private-key=/home/id_rsa -u ansible --ssh-common-args='-o StrictHostKeyChecking=no'
 sleep 330
-aws autoscaling attach-instances --instance-ids $i --auto-scaling-group-name jenkinsasg
+aws elbv2 deregister-targets --target-group-arn arn:aws:elasticloadbalancing:ap-south-1:623240248731:targetgroup/jenkinstg/9adf4a6004cdb56e --targets Id=$i
 done
+
+
 
