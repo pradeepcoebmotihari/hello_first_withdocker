1ce475c8edb45954283d918ecdbb2a8f900cea90
pradeepcoebmotihari <54035127+pradeepcoebmotihari@users.noreply.github.com>
2022-11-30 11:48:30 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -5,7 +5,7 @@ for i in `aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name
 do 
 #aws autoscaling detach-instances --instance-ids $i --auto-scaling-group-name jenkinsasg --should-decrement-desired-capacity
 aws ec2 describe-instances --instance-ids $i |jq -r '.Reservations[].Instances[].PrivateIpAddress' >> /etc/ansible/hosts
-ansible-playbook -e 'host_key_chaking=False' ansible.yml --private-key=/home/id_rsa -u ansible 
+ansible-playbook ansible.yml --private-key=/home/id_rsa -u ansible 
 #sleep 330
 #aws autoscaling attach-instances --instance-ids $i --auto-scaling-group-name jenkinsasg
 done
